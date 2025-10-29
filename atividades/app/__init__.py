67ef3b7d58e146c9650468d5add9b4792905ed45
JoaoVictor2201 <franca.jv@hotmail.com>
2025-10-29 17:32:18 -0300

    atividade service

--- atividades/app/__init__.py
+++ atividades/app/__init__.py
@@ -12,4 +12,10 @@ def create_app(config_class=Config):
     db.init_app(app)
     Swagger(app)
 
+    from app.routes.atividade_routes import atividade_bp
+    app.register_blueprint(atividade_bp)
+    
+    from app.routes.nota_routes import nota_bp
+    app.register_blueprint(nota_bp)
+
     return app
